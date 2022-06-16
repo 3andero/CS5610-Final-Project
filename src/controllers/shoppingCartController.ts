04566640e512e730946687cf75bd81f3c170480f
hyue623 <apricot062333@gmail.com>
2022-06-15 18:23:16 -0700

    fix: cart routing (get) and type issue in model

--- src/controllers/shoppingCartController.ts
+++ src/controllers/shoppingCartController.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction, json, RequestHandler } from "express";
 import { ObjectId } from "mongoose";
 import { Product, ProductModel } from "../models/product";
-import { ShoppingCartModel } from "../models/shoppingCart";
+import { ShoppingCart, ShoppingCartModel } from "../models/shoppingCart";
 
 export const cart_create: RequestHandler<
   {},
@@ -9,38 +9,59 @@ export const cart_create: RequestHandler<
   {
     products: { product_id: ObjectId; quantity: number }[];
   }
-> = async (req, res, next) => {
-  try {
-    // ProductModel.findById(req.body.product_id).exec(
-    //   (err, product_: Product | null) => {
-    //     if (err) {
-    //       return next(err);
-    //     }
-    //     if (product_ === null || product_.quantity < req.body.quantity) {
-    //       return res.send(product_);
-    //     }
-    //     if (product_.quantity < req.body.quantity) {
-    //       return res.send({});
-    //     }
-    //   }
-    // );
-    const data = {
-      products: req.body.products,
-      user_info: req.auth?.sub,
-    };
-    const newCart = await ShoppingCartModel.create(data);
-    return res.status(200).send();
-  } catch (err: any) {
-    return next(err);
-  }
+> = (req, res, next) => {
+  // ProductModel.findById(req.body.product_id).exec(
+  //   (err, product_: Product | null) => {
+  //     if (err) {
+  //       return next(err);
+  //     }
+  //     if (product_ === null || product_.quantity < req.body.quantity) {
+  //       return res.send(product_);
+  //     }
+  //     if (product_.quantity < req.body.quantity) {
+  //       return res.send({});
+  //     }
+  //   }
+  // );
+  const data = {
+    products: req.body.products,
+    user_info: req.auth?.sub,
+  };
+  ShoppingCartModel.find({ user_info: req.auth!.sub }).exec(
+    async (err, item) => {
+      if (err) {
+        return next(err);
+      }
+      // console.log(data);
+      // console.log(JSON.stringify(item));
+      if (item.length === 0) {
+        try {
+          const newCart = await ShoppingCartModel.create(data);
+          return res.status(200).send();
+        } catch (err) {
+          // console.log(err);
+          return next(err);
+        }
+      } else {
+        // console.log("2");
+        const updatedCart = await ShoppingCartModel.updateOne(
+          { user_info: req.auth!.sub },
+          req.body
+        );
+        return res.status(200).send();
+      }
+    }
+  );
+  // console.log("here");
+  // const newCart = await ShoppingCartModel.create(data);
 };
 
 export const cart_get: RequestHandler = (req, res, next) => {
-  ShoppingCartModel.find({ user_info: req.auth!.sub }).exec((err, items) => {
+  ShoppingCartModel.find({ user_info: req.auth!.sub }).exec((err, item) => {
     if (err) {
       return next(err);
     }
-    return res.json(items);
+    return res.json(item);
   });
 };
 
--- src/models/shoppingCart.ts
+++ src/models/shoppingCart.ts
@@ -2,15 +2,28 @@ import typegoose, {
   getModelForClass,
   index,
   modelOptions,
+  Passthrough,
   prop,
   Ref,
   ReturnModelType,
 } from "@typegoose/typegoose";
 import { Product } from "./product";
 import { UserInformation } from "./user_info";
+import * as mongoose from "mongoose";
+
+// class ProductPair{
+//   @prop()
+//   public
+// }
 
 export class ShoppingCart {
-  @prop()
+  @prop({
+    type: () =>
+      new Passthrough({
+        product_id: mongoose.Schema.Types.ObjectId,
+        quantity: Number,
+      }),
+  })
   public products!: { product_id: Ref<Product>; quantity: number }[];
 
   @prop()
