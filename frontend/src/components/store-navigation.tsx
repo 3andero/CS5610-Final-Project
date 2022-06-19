1711d61f6c70d6debedf3b4247117583fa1f72d1
3andero <3andero@github.com>
2022-06-19 14:05:10 -0700

    feat: impl shop (card) & website logo

--- frontend/src/components/store-navigation.tsx
+++ frontend/src/components/store-navigation.tsx
@@ -1,4 +1,11 @@
-import { Box, Typography, Drawer, Button, AppBar } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Drawer,
+  Button,
+  AppBar,
+  useScrollTrigger,
+} from "@mui/material";
 import { useContext } from "react";
 import { AppContext } from "../app-context";
 import { LinkedButton } from "./linked-button";
@@ -6,6 +13,8 @@ import MenuIcon from "@mui/icons-material/Menu";
 import { ShoppingBag } from "@mui/icons-material";
 import { NavLink } from "react-router-dom";
 import { SearchBar } from "./searchbar";
+import { TitleComponent } from "./title";
+import React from "react";
 
 const SideBarAndButton = ({
   onClick,
@@ -50,55 +59,77 @@ const SideBarAndButton = ({
   );
 };
 
+function ElevationScroll({ children }: { children: any }) {
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
 export const StoreNavigation = () => {
   const context = useContext(AppContext);
+
   return (
-    <AppBar
-      position="sticky"
-      sx={{
-        top: 0,
-        bgcolor: "white",
-        color: "grey",
-        padding: "2em",
-        display: "flex",
-        flexDirection: "row",
-        gap: "1em",
-        alignItems: "center",
-        justifyContent: "space-between",
-      }}
-    >
-      <NavLink to="/">
-        <Typography variant="h2">Gen2D</Typography>
-      </NavLink>
-      <Box
+    <ElevationScroll>
+      <AppBar
+        position="sticky"
         sx={{
+          top: 0,
+          bgcolor: "white",
+          color: "grey",
+          padding: "2em",
           display: "flex",
-          justifyContent: "space-evenly",
+          flexDirection: "row",
+          gap: "1em",
           alignItems: "center",
+          justifyContent: "space-between",
         }}
       >
-        <SearchBar />
-        <LinkedButton to={"/profile"} variant="text">
-          Profile
-        </LinkedButton>
-        <LinkedButton to={"/shop"} variant="text">
-          Shop
-        </LinkedButton>
-        <SideBarAndButton
-          onClick={context.toggleSidebar}
-          open={context.sidebarStatus}
-          sx={{ display: { md: "none", xs: "flex" } }}
-          icon={<MenuIcon />}
-          anchor={"left"}
-        />
-        <SideBarAndButton
-          onClick={context.toggleShoppingCart}
-          open={context.shoppingCartStatus}
-          sx={{ display: "flex" }}
-          icon={<ShoppingBag />}
-          anchor={"right"}
-        />
-      </Box>
-    </AppBar>
+        <NavLink
+          to="/"
+          style={{
+            textDecoration: "none",
+          }}
+        >
+          <TitleComponent />
+        </NavLink>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+          }}
+        >
+          <SearchBar />
+          <LinkedButton to={"/profile"} variant="text">
+            Profile
+          </LinkedButton>
+          <LinkedButton to={"/shop"} variant="text">
+            Shop
+          </LinkedButton>
+          <SideBarAndButton
+            onClick={context.toggleSidebar}
+            open={context.sidebarStatus}
+            sx={{ display: { md: "none", xs: "flex" } }}
+            icon={<MenuIcon />}
+            anchor={"left"}
+          />
+          <SideBarAndButton
+            onClick={context.toggleShoppingCart}
+            open={context.shoppingCartStatus}
+            sx={{ display: "flex" }}
+            icon={<ShoppingBag />}
+            anchor={"right"}
+          />
+        </Box>
+      </AppBar>
+    </ElevationScroll>
   );
 };
---
+++ frontend/src/components/title.tsx
@@ -0,0 +1,66 @@
+import { Typography, useTheme } from "@mui/material";
+
+// https://codepen.io/markmead/pen/YjQKeZ
+const TitleComponent1 = () => {
+  const theme = useTheme();
+  return (
+    <Typography
+      variant="h2"
+      fontWeight={"900"}
+      letterSpacing={5}
+      sx={{
+        // -moz-text-stroke-width: $property;
+        WebkitTextStrokeWidth: 2,
+        WebkitTextStrokeColor: "#111827",
+        color: "transparent",
+        textShadow: `0.05em 0.05em ${theme.palette.warning.light}`,
+        fontFamily: "'Catamaran',sans-serif",
+        textTransform: "uppercase",
+      }}
+    >
+      Gen2D
+    </Typography>
+  );
+};
+
+// https://codepen.io/josephwong2004/pen/pogbrOv
+const TitleComponent2 = () => {
+  const text = "'Gen2D'";
+  const pathAfter = "polygon(0 25%, 100% 65%, 100% 100%, 0% 100%)";
+  const pathBefore = "polygon(0 0, 110% 0, 100% 65%, 0 25%)";
+  return (
+    <Typography
+      variant="h2"
+      fontWeight={900}
+      letterSpacing={5}
+      sx={{
+        position: "relative",
+        textTransform: "uppercase",
+        color: "transparent",
+        fontFamily: "'Catamaran', sans-serif",
+        fontStyle: "italic",
+        ":after": {
+          content: text,
+          position: "absolute",
+          left: "-3px",
+          top: "-1px",
+          color: "white",
+          WebkitClipPath: pathAfter,
+          clipPath: pathAfter,
+          bgcolor: "black",
+        },
+        "&:before": {
+          content: text,
+          position: "absolute",
+          color: "black",
+          WebkitClipPath: pathBefore,
+          clipPath: pathBefore,
+        },
+      }}
+    >
+      Gen2D
+    </Typography>
+  );
+};
+
+export const TitleComponent = TitleComponent2;
